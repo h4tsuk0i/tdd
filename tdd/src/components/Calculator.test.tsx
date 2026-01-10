79e88dc57189dc93064c61bdc5b83983953f00a6
h4tsuk0i <lolligerkill@gmail.com>
2026-01-10 17:30:09 +0100

    written initial tests

---
+++ tdd/src/components/Calculator.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Calculator from "./Calculator";
+
+describe("Calculator component", () => {
+  test("renders initial result", () => {
+    render(<Calculator initialResult={0} />);
+    expect(screen.getByText("= 0")).toBeInTheDocument();
+  });
+});
+
+test("adds the two numbers", () => {
+  const result = sum(2, 3);
+  render(<Calculator initialResult={0} />);
+  fireEvent.click(screen.getByRole("button", { name: /result/i }));
+  expect(screen.getByText("=" + " " + result.toString())).toBeInTheDocument();
+});
+
+test("subtracts the two numbers", () => {
+  const result = subtract(2, 3);
+  render(<Calculator initialResult={0} />);
+  fireEvent.click(screen.getByRole("button", { name: /result/i }));
+  expect(screen.getByText("=" + " " + result.toString())).toBeInTheDocument();
+});
+
+test("multiplies the two numbers", () => {
+  const result = multiply(2, 3);
+  render(<Calculator initialResult={0} />);
+  fireEvent.click(screen.getByRole("button", { name: /result/i }));
+  expect(screen.getByText("=" + " " + result.toString())).toBeInTheDocument();
+});
+
+test("divides the two numbers", () => {
+  const result = divide(2, 3);
+  render(<Calculator initialResult={0} />);
+  fireEvent.click(screen.getByRole("button", { name: /result/i }));
+  expect(screen.getByText("=" + " " + result.toString())).toBeInTheDocument();
+});
