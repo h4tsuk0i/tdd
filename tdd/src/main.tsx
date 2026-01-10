d0d7dcad5e27451f2ead079e07d1e8120d100532
h4tsuk0i <lolligerkill@gmail.com>
2026-01-10 16:51:16 +0100

    setup

---
+++ tdd/src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Calculator from "./pages/Calculator.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [{ path: "/", element: <Calculator /> }],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
